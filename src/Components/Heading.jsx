13759d7f9849adc3e859aa3c6ab1c1210a63b776
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 18:56:13 +0600

    apply style on heading

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = () => {
   return (
-    <div className="w-full px-8 md:px-12 flex justify-center mx-auto mt-64">
-      <h1 className="mx-auto  font-bold text-4xl">
+    <div className="w-full px-8 md:px-12 flex justify-center mx-auto mt-78">
+      <h1 className="mx-auto font-bold text-4xl">
         Explore Cutting-Edge Gadgets
       </h1>
     </div>
