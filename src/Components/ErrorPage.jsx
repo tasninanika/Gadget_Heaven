847a2cd70ba28146c31eb31729e750072cac7587
anikatasnin <jarintasnin09@gmail.com>
2025-09-16 14:43:59 +0600

    apply style on error page

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -4,13 +4,21 @@ import errorAnimation from "../assets/error.json";
 
 const ErrorPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-50">
+    <div className="flex flex-col items-center justify-center min-h-screen -mt-32">
       <Helmet>
         <title>404 || Gadget Heaven</title>
       </Helmet>
-      <div className="w-80 md:w-[800px]">
+
+      <div className="w-96 md:w-[800px]">
         <Lottie animationData={errorAnimation} loop={true} />
       </div>
+
+      <h1 className="text-3xl font-bold -mt-48 text-gray-800">
+        Page Not Found
+      </h1>
+      <p className="text-gray-600 mt-2 text-center">
+        Oops! The page you are looking for does not exist.
+      </p>
     </div>
   );
 };
