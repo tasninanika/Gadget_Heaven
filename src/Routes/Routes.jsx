b4e5ccb5ad101a38c3ca264912f81dabb9488044
anikatasnin <jarintasnin09@gmail.com>
2025-09-16 14:25:21 +0600

    apply style on error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -49,6 +49,10 @@ const routes = createBrowserRouter([
         path: "/support",
         element: <Support></Support>,
       },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
     ],
   },
 ]);
